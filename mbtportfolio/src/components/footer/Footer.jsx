d13ad556b0adefb54c1b076d28dcbb8553b0f6fd
Mary Ann Tapiru <maryann.baricante24@gmail.com>
2022-10-03 05:34:27 +0800

    Update

---
+++ mbtportfolio/src/components/footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+        <div className="footer__container container">
+            <h1 className="footer__title">Tapiru</h1>
+
+            <ul className="footer__list">
+                <li>
+                    <a href="#about" className="footer__link">About</a>
+                </li>
+
+                <li>
+                    <a href="#project" className="footer__link">Project</a>
+                </li>
+
+                <li>
+                    <a href="#skills" className="footer__link">Skills</a>
+                </li>
+            </ul>
+
+            <div className="footer__social">
+                <a href="https://www.facebook.com/BaricanteSexy/" className="footer__social-link" target="__blank">
+                <i className="bx bxl-facebook"></i>
+                </a>
+                <a href="https://github.com/" className="footer__social-link" target="__blank">
+                <i className="bx bxl-instagram"></i>
+                </a>
+                <a href="linkedin.com/in/me-ann-tapiru" className="footer__social-link" target="__blank">
+                <i className="bx bxl-linkedin"></i>
+                </a>
+            </div>
+            <span className="footer__copy">
+                &#169; MBTapiru. All rights reserved
+            </span>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer
