ff16bc909537a661ce43dda02c5ad16c972b4299
Mary Ann Tapiru <maryann.baricante24@gmail.com>
2022-10-04 04:34:14 +0800

    Update

---
+++ mbtportfolio copy/src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="linkedin.com/in/me-ann-tapiru" className="home__social-icon" target="__blank">
+          <i className="uil uil-linkedin"></i>
+        </a>
+        <a href="https://github.com/" className="home__social-icon" target="__blank">
+          <i className="uil uil-facebook"></i>
+        </a>
+        <a href="https://github.com/" className="home__social-icon" target="__blank">
+          <i className="uil uil-github"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
---
+++ mbtportfolio copy/src/components/portfolio/Data.jsx
@@ -0,0 +1,58 @@
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Work1,
+        title: 'Front-end',
+        category: 'front-end',
+        url: 'https://www.facebook.com/',
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: 'Full stack',
+        category: 'full stack',
+        url: 'https://www.facebook.com/',
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: 'Wordpress Development',
+        category: 'wordPress',
+        url: 'https://www.facebook.com/',
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: 'Wordpress Development',
+        category: 'wordPress',
+        url: 'https://www.facebook.com/',
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: 'Wordpress Development',
+        category: 'wordPress',
+        url: 'https://www.facebook.com/',
+    },
+];
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'front-end',
+    },
+    {
+        name: 'full stack',
+    },
+    {
+        name: 'wordPress',
+    },
+];
